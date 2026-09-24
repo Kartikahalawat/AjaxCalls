9c90c5a316f145352b48f791bcd5812562fc0764
KARTIK AHALAWAT <kartikahalawat01@gmail.com>
2026-09-24 21:43:06 +0530

    Action class Implementation

--- src/main/java/AjaxCallsTesting.java
+++ src/main/java/AjaxCallsTesting.java
@@ -1,8 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import java.time.Duration;
 
 public class AjaxCallsTesting {
     static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        WebElement move = driver.findElement(By.cssSelector("[id='nav-link-accountList']"));
+
+        Actions act = new Actions(driver);
+
+        act.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+
+        act.moveToElement(move).contextClick().build().perform();
+
     }
 }
